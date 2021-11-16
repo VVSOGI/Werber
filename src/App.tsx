1ff66255d49756b134594b7436f6a4b96ca30051
vvsoggy <vvsogi@gmail.com>
2021-11-16 18:03:30 +0900

    feat/ 기본 구성

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Mainpage from "./pages/Mainpage";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Mainpage />
+    </>
+  );
+};
+
+export default App;
